621a52b53368523ef471430251cc6bc3344078bf
Andreas Holmström <66074771+AtomicPuffin@users.noreply.github.com>
2023-12-01 08:10:38 +0100

    Day 1

--- day1_rust/src/main.rs
+++ day1_rust/src/main.rs
@@ -1,4 +1,5 @@
-use std::fs;
+//use itertools::Itertools;
+use std::{char, fs};
 
 fn main() {
     println!(
@@ -8,18 +9,72 @@ fn main() {
     println!("Answer to Part 1: {}", part_1(&read_file("input.txt")));
     println!(
         "Answer to Part 2 test: {}",
-        part_2(&read_file("example.txt"))
+        part_2(&read_file("example2.txt"))
     );
     println!("Answer to Part 2: {}", part_2(&read_file("input.txt")));
 }
 
 fn part_1(input: &str) -> i64 {
-    todo!()
+    let mut sum = 0;
+    let mut first: char = '0';
+    let mut last: char = '0';
+    let mut found_one = false;
+    for line in input.lines() {
+        for char in line.chars() {
+            if char.is_numeric() && !found_one {
+                first = char;
+                last = char;
+                found_one = true;
+            } else if char.is_numeric() {
+                last = char;
+            }
+        }
+        let num = (first.to_string() + &last.to_string())
+            .parse::<i64>()
+            .unwrap();
+        sum += num;
+        found_one = false;
+    }
+    sum
 }
 
 fn part_2(input: &str) -> i64 {
-    //let lines: Vec<i64> = input.lines().map(|x| x.parse::<i64>().unwrap()).collect();
-    todo!()
+    let _units = vec![
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let mut sum = 0;
+    let mut first: char = '0';
+    let mut last: char = '0';
+    let mut found_one = false;
+    for line in input.lines() {
+        let mut newline: String;
+        //written numbers can overlap - eigh|t|wo so injection cannot break this
+        newline = line.replace("zero", "ze0ro");
+        newline = newline.replace("one", "o1ne");
+        newline = newline.replace("two", "t2wo");
+        newline = newline.replace("three", "th3ree");
+        newline = newline.replace("four", "fo4ur");
+        newline = newline.replace("five", "f5ve");
+        newline = newline.replace("six", "si6x");
+        newline = newline.replace("seven", "sev7en");
+        newline = newline.replace("eight", "eig8ht");
+        newline = newline.replace("nine", "ni9ne");
+        for char in newline.chars() {
+            if char.is_numeric() && !found_one {
+                first = char;
+                last = char;
+                found_one = true;
+            } else if char.is_numeric() {
+                last = char;
+            }
+        }
+        let num = (first.to_string() + &last.to_string())
+            .parse::<i64>()
+            .unwrap();
+        sum += num;
+        found_one = false;
+    }
+    sum
 }
 
 fn read_file(file: &str) -> String {
@@ -33,21 +88,21 @@ mod tests {
 
     #[test]
     fn test_p1_ex() {
-        assert_eq!(part_1(&read_file("example.txt")), todo!());
+        assert_eq!(part_1(&read_file("example.txt")), 142);
     }
 
     #[test]
     fn test_p1() {
-        assert_eq!(part_1(&read_file("input.txt")), todo!());
+        assert_eq!(part_1(&read_file("input.txt")), 55130);
     }
 
     #[test]
     fn test_p2_ex() {
-        assert_eq!(part_2(&read_file("example.txt")), todo!());
+        assert_eq!(part_2(&read_file("example2.txt")), 281);
     }
 
     #[test]
     fn test_p2() {
-        assert_eq!(part_2(&read_file("input.txt")), todo!());
+        assert_eq!(part_2(&read_file("input.txt")), 54985);
     }
 }

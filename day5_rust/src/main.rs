c65e5ebe9019c489dbed3a3864ee654bd14f1163
Andreas Holmström <66074771+AtomicPuffin@users.noreply.github.com>
2023-12-05 14:28:16 +0100

    Redid part 5 for faster runtime, no longer brute force alg

--- day5_rust/src/main.rs
+++ day5_rust/src/main.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use itertools::Itertools;
 use std::fs;
 
@@ -9,9 +10,9 @@ fn main() {
     println!("Answer to Part 1: {}", part_1(&read_file("input.txt")));
     println!(
         "Answer to Part 2 test: {}",
-        part_2(&read_file("example.txt"))
+        part_2_opt(&read_file("example.txt"))
     );
-    println!("Answer to Part 2: {}", part_2(&read_file("input.txt")));
+    println!("Answer to Part 2: {}", part_2_opt(&read_file("input.txt")));
 }
 
 fn part_1(input: &str) -> i64 {
@@ -58,6 +59,88 @@ fn part_2(input: &str) -> i64 {
     lowest
 }
 
+fn part_2_opt(input: &str) -> i64 {
+    //remade for fast runtime
+    let mut lowest = i64::MAX;
+    let (seeds, sections) = do_the_line(input);
+    let mut _pairs = seeds.iter().peekable();
+    let mut pairs = Vec::new();
+    //create seed ranges, start, stop, and shifted flag to ensure we only shift once per section
+    while _pairs.len() > 0 {
+        let (start, range) = (_pairs.next().unwrap(), _pairs.next().unwrap());
+        let seed = (*start, start + range - 1, false);
+        pairs.push(seed);
+    }
+    let mut maps = Vec::new();
+    for sec in sections.iter() {
+        let mut convert_map = Vec::new();
+        for row in sec.iter() {
+            let distance = row[0] - row[1];
+            let start = row[1];
+            let end = row[1] + row[2] - 1;
+            convert_map.push((start, end, distance));
+        }
+        maps.push(convert_map);
+    }
+    //To ensure alignment, flip execution and step through maps, then rows instead of starting with seeds
+    for cmap in maps.iter() {
+        for row in cmap.iter() {
+            let mut new_pairs = Vec::new();
+            //iterate through all seeds, if they overlap the row, split and shift the overlapping part, mark shifted
+            for seeds in pairs.iter_mut() {
+                if seeds.2 {
+                    //see if already shifted, if so pass along
+                    new_pairs.push(seeds.clone());
+                } else if seeds.1 < row.0 || seeds.0 > row.1 {
+                    //no overlap, do nothing, push seed to next row
+                    new_pairs.push(seeds.clone());
+                } else if seeds.0 >= row.0 && seeds.1 <= row.1 {
+                    //full overlap, shift all
+                    seeds.0 += row.2;
+                    seeds.1 += row.2;
+                    seeds.2 = true;
+                    new_pairs.push(seeds.clone());
+                } else if seeds.0 <= row.0 && seeds.1 <= row.1 {
+                    //partial overlap low, split and shift overlapping
+                    let seeds1 = (seeds.0, row.0 - 1, false);
+                    let seeds2 = (row.0 + row.2, seeds.1 + row.2, true);
+                    new_pairs.push(seeds1);
+                    new_pairs.push(seeds2);
+                } else if seeds.0 >= row.0 && seeds.1 >= row.1 {
+                    //partial overlap high, split and shift overlapping
+                    let seeds1 = (seeds.0 + row.2, row.1 + row.2, true);
+                    let seeds2 = (row.1 + 1, seeds.1, false);
+                    new_pairs.push(seeds1);
+                    new_pairs.push(seeds2);
+                } else if seeds.0 < row.0 && seeds.1 > row.1 {
+                    //full overlap low high, split and shift overlapping
+                    let seeds1 = (seeds.0, row.0 - 1, false);
+                    let seeds2 = (row.0 + row.2, row.1 + row.2, true);
+                    let seeds3 = (row.1 + 1, seeds.1, false);
+                    new_pairs.push(seeds1);
+                    new_pairs.push(seeds2);
+                    new_pairs.push(seeds3);
+                } else {
+                    println!("row {} {} {}", row.0, row.1, row.2);
+                    println!("seeds {} {} {}", seeds.0, seeds.1, seeds.2);
+                    panic!("Shouldn't be here");
+                }
+            }
+            pairs = new_pairs.clone();
+        }
+        for seeds in pairs.iter_mut() {
+            //reset shifted flag before next section
+            seeds.2 = false;
+        }
+    }
+    for seeds in pairs.iter() {
+        if seeds.0 < lowest {
+            lowest = seeds.0;
+        }
+    }
+    lowest
+}
+
 fn do_the_line(input: &str) -> (Vec<i64>, Vec<Vec<Vec<i64>>>) {
     let _sections = input.split("\n\n").collect_vec();
     let _seeds = _sections[0].split(' ').collect_vec()[1..].to_vec();
@@ -69,7 +152,9 @@ fn do_the_line(input: &str) -> (Vec<i64>, Vec<Vec<Vec<i64>>>) {
         //println!("seed: {}", s);
         seeds.push(s.parse::<i64>().unwrap());
     }
-    for sec in _sections {
+    seeds.push(82);
+    seeds.push(1);
+    for sec in _sections[1..].iter() {
         let mut section = Vec::new();
         for line in sec.lines().skip(1) {
             let nums = line
